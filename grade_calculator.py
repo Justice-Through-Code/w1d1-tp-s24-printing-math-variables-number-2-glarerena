0cbee2c2a8b7796263061d1bd10224a88cc6abb9
Rena Glare <rena.glare@icloud.com>
2024-05-13 22:32:24 -0400

    Completed the grade average challenge

--- grade_calculator.py
+++ grade_calculator.py
@@ -1,15 +1,18 @@
 def calculate_average_grade():
     # Prompt the user for their name and store it in the student_name variable
-    student_name = ""
+    student_name = input("Enter your name")
 
     # Prompt the user for their scores in Math, Science, and English
     # Store the scores in the respective variables: math_score, science_score, english_score
-    math_score = 0
-    science_score = 0
-    english_score = 0
+    math_score = int(input("Enter your Math Score: "))
+    science_score = int(input("Enter your Science Score: "))
+    english_score = int(input("Enter your English Score: "))
+
 
     # Calculate the average grade
-    average_grade = 0
+    # use the PEDMAS order to make sure the grades were added first
+    average_grade = (math_score + science_score + english_score) / 3
+    
 
     # Return the student's name and their average grade
     return student_name, average_grade
@@ -19,4 +22,5 @@ if __name__ == '__main__':
     student_name, average_grade = calculate_average_grade()
 
     # Print the student's name and their average grade
-    print(f"")
+    print(f"Student's name: {student_name}, average grade: {average_grade}")
+
